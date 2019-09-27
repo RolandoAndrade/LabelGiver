94180541264935afdc1a8af3aec772c38a4d17af
rolandoandrade <rolandoandradefernandez@gmail.com>
2019-09-27 19:38:30 -0400

    Añadidos más datos para modelo

--- main.cpp
+++ main.cpp
@@ -37,8 +37,26 @@ void addToMap(map<string,int> &m, vector<string> words)
 {
 	for(string s: words)
 	{
-		m[s]++;
+		if(s.size()>3)
+			m[s]++;
 	}
+}
+
+int searchMin(vector<pair<string, int>> v, int value)
+{
+	int i = 0;
+	for(; i < v.size()&&v[i].second>value; i++);
+	return i;
+}
+
+vector<pair<string, int>> results(map<string, int> m)
+{
+	vector<pair<string, int>> r;
+	for(auto &q: m)
+	{
+		r.insert(r.begin()+searchMin(r,q.second), q);
+	}
+	return r;
 }
 
 int main(int argc, char const *argv[])
@@ -52,10 +70,15 @@ int main(int argc, char const *argv[])
 		addToMap(m, getWords(s));
 		i++;
 	}
-	cout<<i<<endl;
-	for(auto &t: m)
+	cout<<"Leídos "<<i<<" ejemplos"<<endl;
+
+	vector<pair<string, int>> r = results(m);
+	int j = 0;
+	for(auto &t: r)
 	{
-		cout<<t.first<<" = "<<t.second<<endl;
+		cout<<t.first<<" = "<<t.second<<", "<<(t.second*100/i)<<"%"<<endl;
+		j++;
 	}
+	cout<<"Total de palabras extraídas "<<j<<endl;
 	return 0;
 }
